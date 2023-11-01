aed319090efb8edc5ca6e8204b97209aeccef88b
nmulloy <nmulloy@tcd.ie>
2023-11-01 21:16:42 +0000

    updated buttons and searchbar

---
+++ App.js
@@ -0,0 +1,8 @@
+import {Text, View } from 'react-native';
+import Home from './screens/Home';
+
+export default function App() {
+  return <Home />;
+}
+
+
---
+++ screens/Home.js
@@ -0,0 +1,22 @@
+import { View, Text, SafeAreaView, Platform } from 'react-native'
+import React from 'react'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+
+
+export default function Home() {
+  return (
+    <SafeAreaView style = {{
+      flex: 1, 
+      paddingTop: Platform.OS === 'android' ? 75 : 0,
+      backgroundColor: "#eee"
+      }}> 
+      <View style= {{ backgroundColor: "white", padding: 15}}>
+      <HeaderTabs />
+      <SearchBar />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+  
