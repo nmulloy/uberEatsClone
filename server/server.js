4290b76ee187eb10c01505deb7495ce2d5e1eb12
nmulloy <nmulloy@tcd.ie>
2023-11-08 13:00:40 +0000

    added hidden API key

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 var cors = require('cors')
+
 app.use(cors())
-
 app.use(express.json({ limit: '1mb' })); // Body parsing middleware should come first.// Serve static files.
-
 app.listen(3000, '192.168.68.112', () => console.log("Listening at 192.168.68.112:3000"));
+require('dotenv').config();
 
-const YELP_API_KEY = "SNYhAQ6vYQrELQIdT3c9OgTDix2BWkcTdXcmRS6uDS9FrPxjCAnjKeglh8LgjZg4ysCrpLdNOPHKUxoTDGTEzu0iC-8yVOXjLIbCPrIUCElW0i7JVcbIcfYRDN9DZXYx"
+const YELP_API_KEY = process.env.YELP_API_KEY;
+console.log(process.env)
+console.log(YELP_API_KEY)
+
 const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=Dublin';
 const apiOptions ={
     headers:{
